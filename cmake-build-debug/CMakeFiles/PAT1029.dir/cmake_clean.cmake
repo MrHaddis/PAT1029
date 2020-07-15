f2912da06d820d8f47d35b2335b5e9234b8ef917
duanyuxiang <duanyx@belink.com>
2020-07-15 15:59:01 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/PAT1029.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PAT1029.dir/main.c.obj"
+  "PAT1029.exe"
+  "PAT1029.exe.manifest"
+  "PAT1029.pdb"
+  "libPAT1029.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PAT1029.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
